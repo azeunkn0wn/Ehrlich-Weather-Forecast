c770ba0e3ac425407cf46de5529f5c0067c0fd51
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-01 02:28:33 +0800

    feat landing page decides if user requires login if user have stored login credentials, landing page will navigate to HomeScreen, else LoginScreen

---
+++ lib/landing_page.dart
@@ -0,0 +1,46 @@
+import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_forcast/api/auth_api_controller.dart';
+import 'package:weather_forcast/home_screen.dart';
+import 'package:weather_forcast/login_screen.dart';
+
+class LandingPage extends StatefulWidget {
+  LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  AuthController authController = AuthController();
+
+  late Future<bool> userLoginStatus;
+
+  @override
+  void initState() {
+    super.initState();
+    userLoginStatus = authController.isStored();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: userLoginStatus,
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        //TODO Issue: Future Builder not working as intended. This should load stored credentials and skip login page.
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            var credentialsAreStored = snapshot.data!;
+            if (credentialsAreStored) {
+              return UserWidget(user: authController.user);
+            }
+          }
+          return LoginScreen(authController: authController);
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
