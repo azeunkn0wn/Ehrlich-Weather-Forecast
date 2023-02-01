255c91d4ec12d5ad641fa34d1c007ddcbdf6489c
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-01 18:39:23 +0800

    add back button should exit instead of going back

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:weather_forcast/controller/auth_api_controller.dart';
 import 'package:weather_forcast/home_screen.dart';
 
@@ -42,38 +43,44 @@ class _LoginScreenState extends State<LoginScreen> {
     var margin = EdgeInsets.symmetric(
       horizontal: MediaQuery.of(context).size.width * 0.15,
     );
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.cloud),
-        title: const Text('Weather Forecast'),
-      ),
-      body: Column(
-        children: [
-          const Padding(padding: EdgeInsets.only(bottom: 100)),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-                margin: margin,
-                child: const Text(
-                  landingPageMessage,
-                  softWrap: true,
-                  maxLines: 5,
-                  textAlign: TextAlign.left,
-                )),
-          ),
-          const Padding(padding: EdgeInsets.only(bottom: 30)),
-          Container(
-            margin: margin,
-            child: Row(
-              children: [
-                ElevatedButton(
-                  onPressed: _login,
-                  child: const Text("Log in"),
-                ),
-              ],
+    return WillPopScope(
+      onWillPop: () async {
+        SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.cloud),
+          title: const Text('Weather Forecast'),
+        ),
+        body: Column(
+          children: [
+            const Padding(padding: EdgeInsets.only(bottom: 100)),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                  margin: margin,
+                  child: const Text(
+                    landingPageMessage,
+                    softWrap: true,
+                    maxLines: 5,
+                    textAlign: TextAlign.left,
+                  )),
             ),
-          ),
-        ],
+            const Padding(padding: EdgeInsets.only(bottom: 30)),
+            Container(
+              margin: margin,
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: const Text("Log in"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
