13814dda91216359dadc3c6d0afa0bef43e578b7
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-04 00:16:24 +0800

    refactor apikey moved to .env

--- lib/services/weather_forecast_constants.dart
+++ lib/services/weather_forecast_constants.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class WeatherForecastApiConstants {
   static String baseUrl = 'http://api.openweathermap.org';
-  static String apiKey = '9868dbc5f1d48a2099e663ab701bc5cf';
+  static String apiKey = dotenv.env['WEATHER_API_KEY']!;
   static String weatherEndpoint = '/data/2.5/weather';
 }
